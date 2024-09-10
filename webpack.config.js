8adbe9896841e43676fbfd5a93e86051ca79be3e
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-09-10 15:54:35 -0700

    Edit module.rules array to support image types

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|webp)$/i,
         type: "asset/resource",
       }
     ],

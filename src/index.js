211542c7dad6f3f2f29e205b1cee84d2a6f7d1f7
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-09-11 14:42:24 -0700

    Complete logic for tab switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 // index.js
-import homepageLoad from "./homepage.js";
+import showHomePage from "./homepage.js";
+import showMenu from "./menu.js";
 
-window.addEventListener("load", () => homepageLoad());
+window.addEventListener("load", () => showHomePage());
+
+const navBtns = document.querySelectorAll("nav button"); 
+navBtns.forEach(btn => btn.addEventListener("click", (event) => {
+  if(event.target.classList.contains("selected")) {
+    console.log("pressed same button. do nothing"); 
+    return;
+  }
+  const currentBtn = document.querySelector("button.selected"); 
+  currentBtn.classList.remove("selected");
+  event.target.classList.add("selected");
+  const content = document.querySelector("#content"); 
+  content.textContent = "";
+  if(event.target.textContent === "Home") {
+    showHomePage();
+  } else if(event.target.textContent === "Menu") {
+    showMenu(); 
+  } else {
+    console.log("not done");
+  }
+})); 
